db610be162ace518acc21b9ffe5ee76314d2abb7
eduardo <eduregis1@gmail.com>
2024-12-19 21:36:22 -0300

    scroll text with complete tap

--- Assets/Scripts/Gameplay/ScrollText.cs
+++ Assets/Scripts/Gameplay/ScrollText.cs
@@ -1,17 +1,47 @@
 // ScrollText.cs
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using System.Collections;
 
 public class ScrollText : MonoBehaviour {
     public TextMeshProUGUI textDisplay;  
     public float typingSpeed = 0.05f;
-
-    // Referência para o UIManager, ou pode ser referência ao GameStateManager
     public UIManager uiManager;
+    private Coroutine typingCoroutine;
+    private string fullText;
+    private bool isTyping = false;
 
+    private void Awake() {
+        if (textDisplay != null) {
+            textDisplay.raycastTarget = true;
+        }
+    }
+
+    private void Update() {
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began) {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                if (Physics.Raycast(ray, out RaycastHit hit)) {
+                    if (hit.transform == transform) {
+                        OnPointerClick();
+                    }
+                }
+            }
+        }
+    }
+
+    
     public void TypeText(string text) {
-        StartCoroutine(TypeRoutine(text));
+        fullText = text;
+        isTyping = true;
+
+        if (typingCoroutine != null) {
+            StopCoroutine(typingCoroutine);
+        }
+
+        typingCoroutine = StartCoroutine(TypeRoutine(text));
     }
 
     private IEnumerator TypeRoutine(string text) {
@@ -20,8 +50,26 @@ public class ScrollText : MonoBehaviour {
             textDisplay.text += letter;
             yield return new WaitForSeconds(typingSpeed);
         }
-        
-        // Chama o método de flipar as cartas após terminar a digitação
+        isTyping = false;
+        typingCoroutine = null;
+        OnTypingComplete();
+    }
+
+    public void CompleteTyping() {
+        if (isTyping) {
+            StopCoroutine(typingCoroutine);
+            textDisplay.text = fullText;
+            isTyping = false;
+            typingCoroutine = null;
+            OnTypingComplete();
+        }
+    }
+
+    private void OnTypingComplete() {
         uiManager.FlipCards();
+    }
+
+    public void OnPointerClick() {
+        CompleteTyping();
     }
 }
